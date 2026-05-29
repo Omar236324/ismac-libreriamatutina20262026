5a44a44c99eb6cbb6dbb5a9b3108c822b1cc2021
1726449182 <oyumisaca@tecnologicoismac.edu.ec>
2026-05-29 12:55:59 -0500

    trabajo conexion con base de base clase arreglo conexion autor, categora

--- src/main/java/com/distribuida/dao/CategoriaDAO.java
+++ src/main/java/com/distribuida/dao/CategoriaDAO.java
@@ -1,10 +1,10 @@
-//package com.distribuida.dao;
-//
-//import com.distribuida.model.Categoria;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//@Repository
-//public interface CategoriaDAO extends JpaRepository<Categoria,Integer>{
-//
-//    Categoria findCategoria(String categoria);
-//}
+package com.distribuida.dao;
+
+import com.distribuida.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface CategoriaDAO extends JpaRepository<Categoria,Integer>{
+
+    Categoria findCategoria(String categoria);
+}
--- src/test/java/com/distribuida/model/AutorTest.java
+++ src/test/java/com/distribuida/model/AutorTest.java
@@ -1,40 +1,40 @@
-//package com.distribuida.model;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//public class AutorTest {
-//    private Autor autor;
-//
-//    @BeforeEach
-//    public void setUp(){
-//        autor = new Autor(
-//                1,
-//                "Juan",
-//                "Rodriguez",
-//                "Ecuador",
-//                "pifo",
-//                "0987654321",
-//                "Rodrigop@gmail.com"
-//        );
-//    }
-//    @Test
-//    public void testClientConstructorAndGetters(){
-//        assertAll("Pruebas unitarias constructor Autor",
-//                () ->assertEquals(1,autor.getIdAutor()),
-//                () ->assertEquals("pedor",autor.getNombre()),
-//                () ->assertEquals("Rodriguez",autor.getApellido()),
-//                () ->assertEquals("Ecuador",autor.getPais()),
-//                () ->assertEquals("pifo",autor.getDireccion()),
-//                () ->assertEquals("Rodrigop@gmail.com",autor.getCorreo()),
-//                () ->assertEquals("0987654321",autor.getTelefono())
-//
-//
-//        );
-//    }
-//
-//
-//}
+package com.distribuida.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AutorTest {
+    private Autor autor;
+
+    @BeforeEach
+    public void setUp(){
+        autor = new Autor(
+                1,
+                "Juan",
+                "Rodriguez",
+                "Ecuador",
+                "pifo",
+                "0987654321",
+                "Rodrigop@gmail.com"
+        );
+    }
+    @Test
+    public void testClientConstructorAndGetters(){
+        assertAll("Pruebas unitarias constructor Autor",
+                () ->assertEquals(1,autor.getIdAutor()),
+                () ->assertEquals("pedor",autor.getNombre()),
+                () ->assertEquals("Rodriguez",autor.getApellido()),
+                () ->assertEquals("Ecuador",autor.getPais()),
+                () ->assertEquals("pifo",autor.getDireccion()),
+                () ->assertEquals("Rodrigop@gmail.com",autor.getCorreo()),
+                () ->assertEquals("0987654321",autor.getTelefono())
+
+
+        );
+    }
+
+
+}
